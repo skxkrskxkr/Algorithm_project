5e03489b5e15360c42cbfb22be9e4418d4e2fe6d
DESKTOP-MABOA5M\taegu <taegun89@hotmail.com>
2018-05-16 19:24:25 +0900

    + shell_sort

--- algor_practice/Project9/p_algo.cpp
+++ algor_practice/Project9/p_algo.cpp
@@ -64,14 +64,45 @@ void insert_sort(int *arr, int arr_size) {
 	}
 }
 
+void shell_sort(int *arr, int arr_size) {
+	int h = 1; int temp; int temp_index;
+
+	do {
+		h = 3 * h + 1;
+	} while (h < arr_size);
+
+	do {
+		h = h / 3;
+
+		for (int i = h; i < arr_size; i++) {
+			temp = arr[i];
+			temp_index = i;
+
+
+			while (arr[temp_index - h] > temp) {
+				arr[temp_index] = arr[temp_index - h];
+				temp_index -= h;
+
+				if (temp_index < h - 1) break;
+			}
+			arr[temp_index] = temp;
+		}
+		for (int i = 0; i < arr_size; i++) {
+			std::cout << arr[i] << " ";
+		}
+		std::cout << std::endl;
+	} while (h > 1);
+
+}
 int main() {
 
-	int arr[8] = { 30, 20, 40, 10, 5, 10, 30, 15 };
-	int arr_size = 8;
+	int arr[15] = { 30, 20, 40, 10, 5, 10, 30, 15 , 27, 35, 80, 22, 12, 5, 29};
+	int arr_size = 15;
 
 //	Selection_sort(arr, arr_size);
 //	Bubble_sort(arr, arr_size);
-	insert_sort(arr, arr_size);
+//	insert_sort(arr, arr_size);
+	shell_sort(arr, arr_size);
 	for (int i = 0; i < arr_size; i++) {
 		std::cout << arr[i] << " ";
 	}
